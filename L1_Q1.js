e7d68c6feae21b6a792296c9b6b4033fc3304d54
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-07 15:32:20 +0800

    Create L1_Q1.js

---
+++ L1_Q1.js
@@ -0,0 +1,14 @@
+/**
+ * Topic: Variables
+ * Challenge: Easy
+ * Question 1
+ */
+let university = "NUS";
+const cca = "The Navigators";
+
+// cca = "something else";  // This is not allowed
+
+university = "NTU";
+
+console.log(university); // NTU
+console.log(cca); // The Navigators
