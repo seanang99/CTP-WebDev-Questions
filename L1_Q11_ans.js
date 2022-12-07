a0321873a811be7866e26d7549dd18413300f685
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-07 17:56:16 +0800

    Create L1_Q11_ans.js

---
+++ L1_Q11_ans.js
@@ -0,0 +1,16 @@
+/**
+ * Topic: Arrays, Loops
+ * Challenge: Easy
+ * Question 11
+ */
+
+ function reverse(word) {
+  let result = "";
+  for (let i = word.length - 1; i > -1; i--) {
+      result = result + word[i];
+  }
+  return result;
+}
+
+console.log(reverse("sailor"));
+console.log(reverse("racecar"));
