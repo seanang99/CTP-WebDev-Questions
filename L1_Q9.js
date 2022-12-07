0bdad062d407b73d94c8068d99207df16c9c52f6
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-07 15:41:41 +0800

    Create L1_Q9.js

---
+++ L1_Q9.js
@@ -0,0 +1,18 @@
+/**
+ * Topic: Array, Loop, Math
+ * Challenge: Medium
+ * Question 9
+ */
+
+ var fizzBuzz = function(n) {
+  const output = [];
+  for(let i = 1; i <= n; i++) {
+      let input = "";
+      if  (i % 3 == 0)
+          input += "Fizz";
+      if (i % 5 == 0)
+          input += "Buzz";
+      output.push(input ? input : i.toString());
+  }
+  return output;
+};
