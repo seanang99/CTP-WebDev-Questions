ad005d08d900baa2d87c2b7c6bf76da3428a81a9
Sean <54630058+seanang99@users.noreply.github.com>
2022-12-07 15:33:54 +0800

    Update L1_Q1.js

--- L1_Q1.js
+++ L1_Q1.js
@@ -3,12 +3,5 @@
  * Challenge: Easy
  * Question 1
  */
-let university = "NUS";
-const cca = "The Navigators";
 
-// cca = "something else";  // This is not allowed
-
-university = "NTU";
-
-console.log(university); // NTU
-console.log(cca); // The Navigators
+ var firstName = "John", lastName = "Doe", age = 35;
